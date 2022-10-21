062819cde85dd151ff5376ace60344e1de670234
jftierno <jftierno@gmail.com>
2022-10-21 01:31:54 -0300

    SignUp and Login working

--- app/src/main/java/com/example/figutrader/ui/Registro/RegistroFragment.kt
+++ app/src/main/java/com/example/figutrader/ui/Registro/RegistroFragment.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationAPIClient
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.callback.Callback
+import com.auth0.android.result.Credentials
 import com.example.figutrader.R
 import com.example.figutrader.databinding.FragmentRegistrarseBinding
-import com.example.figutrader.ui.registro.RegistroViewModel
 
 class RegistroFragment : Fragment() {
 
@@ -19,6 +24,19 @@ class RegistroFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    val account = Auth0("OisvgmujDA8VxbbBdvbI1h7itTe2OgjD", "dev-1v0y0xnrlbzoiled.us.auth0.com")
+    val authenticationApi = AuthenticationAPIClient(account)
+    val signUpCallback = object : Callback<Credentials, AuthenticationException> {
+        override fun onFailure(exception: AuthenticationException) {
+            findNavController().navigate(R.id.nav_registro)
+        }
+
+        override fun onSuccess(credentials: Credentials) {
+            findNavController().navigate(R.id.nav_login)
+        }
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,7 +65,16 @@ class RegistroFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonSignup2.setOnClickListener {
-            findNavController().navigate(R.id.nav_login)
+            val password = view.findViewById<EditText>(R.id.inputPasswordSignup).text.toString()
+            val passwordConfirm = view.findViewById<EditText>(R.id.inputConfirmPasswordSignup).text.toString()
+            val username = view.findViewById<EditText>(R.id.inputUsernameSignup).text.toString()
+            val email = view.findViewById<EditText>(R.id.inputEmailSignup).text.toString()
+            if(password == passwordConfirm) {
+                authenticationApi
+                    .signUp(email,password,username,"Username-Password-Authentication")
+                    .validateClaims()
+                    .start(signUpCallback)
+            }
         }
     }
 }
--- app/src/main/java/com/example/figutrader/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/figutrader/ui/login/LoginFragment.kt
@@ -1,13 +1,20 @@
 package com.example.figutrader.ui.login
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationAPIClient
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.callback.Callback
+import com.auth0.android.result.Credentials
 import com.example.figutrader.R
 import com.example.figutrader.databinding.FragmentLoginBinding
 
@@ -18,6 +25,19 @@ class LoginFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    val account = Auth0("OisvgmujDA8VxbbBdvbI1h7itTe2OgjD", "dev-1v0y0xnrlbzoiled.us.auth0.com")
+    val authenticationApi = AuthenticationAPIClient(account)
+    val loginCallback = object : Callback<Credentials, AuthenticationException> {
+        override fun onFailure(exception: AuthenticationException) {
+            Log.e("leonE",exception.getDescription())
+            findNavController().navigate(R.id.nav_login)
+        }
+
+        override fun onSuccess(credentials: Credentials) {
+            findNavController().navigate(R.id.nav_menu_principal)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,8 +63,6 @@ class LoginFragment : Fragment() {
         _binding = null
     }
 
-
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -53,7 +71,12 @@ class LoginFragment : Fragment() {
         }
 
         binding.buttonLogin.setOnClickListener {
-            findNavController().navigate(R.id.nav_menu_principal)
+            val password = view.findViewById<EditText>(R.id.inputPasswordLogin).text.toString()
+            val username = view.findViewById<EditText>(R.id.inputUserLogin).text.toString()
+            authenticationApi
+                .login(username,password,"Username-Password-Authentication")
+                .validateClaims()
+                .start(loginCallback)
         }
     }
 }
